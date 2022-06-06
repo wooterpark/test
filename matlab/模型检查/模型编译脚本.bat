fde6dec761c7140246b52768f0f93e8c9f38467b
zhaozhongyi <zhaozhongyi@jee-cn.com>
2022-06-06 19:53:12 +0800

    <commit message>

--- matlab/模型检查/模型编译脚本.bat
+++
@@ -1,5 +0,0 @@
-@echo off&&setlocal enabledelayedexpansion
-matlab -nojvm -nodesktop -nodisplay -r "run('G:\zzy\project\config.m')"
-::注意修改config.m地址
-
-::taskkill /f  /im matlab.exe
--- matlab/模型检查并编译.bat
+++
@@ -1,3 +0,0 @@
-@echo off&&setlocal enabledelayedexpansion
-matlab -nojvm -nodesktop -nodisplay -r "run('C:\Users\zhaozhongyi\Desktop\Model\SWD\matlab.m')"
-::注意修改compile.m地址，compile.m存放于你想要的模型路径下
