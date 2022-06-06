8a390eedc201ac84a1b866188fb709baf275dfce
zhaozhongyi <zhaozhongyi@jee-cn.com>
2022-06-06 16:28:12 +0800

    <commit message>

--- Model/03_ASW_Configuration/01_m/iEDS_Enum_stSysCtl_def.m
+++
@@ -1,9 +0,0 @@
-classdef iEDS_Enum_stSysCtl_def< Simulink.IntEnumType
-    enumeration
-        PreDriveMod (0)
-        DriveMod (1)
-        PostDriveMod (2)
-        ShutDownMod (3)
-        FaultMod (4)
-    end
-end
