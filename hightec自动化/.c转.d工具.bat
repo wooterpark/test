776e3e214428ed34894e528a7c5f7b513e3d28c8
zhaozhongyi <zhaozhongyi@jee-cn.com>
2022-06-07 08:07:51 +0800

    <commit message>

--- hightec自动化/.c转.d工具.bat
+++
@@ -1,6 +0,0 @@
-@echo off&&setlocal enabledelayedexpansion
-for /f "tokens=*" %%i in (subdir1.txt) do (
-set var=%%i
-set "var=!var:.c=.d \!"
-echo !var!>>subdir4.txt
-)
--- hightec自动化/调用.bat
+++
@@ -1,9 +0,0 @@
-@echo off&&setlocal enabledelayedexpansion
-for /f "tokens=*" %%i in (list3.txt) do (
-   set var=%%i
-   copy G:\zzy\doc\Hightec\FS_PEB_0225\first.bat !%%i!
-   ::修改找first.bat
-    cd !%%i!
-	 call first.bat
-	 del first.*
-)
